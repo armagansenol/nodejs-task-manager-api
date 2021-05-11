d5fe1ab9e1ff12228b24624c5b32124f0c3fe820
armagansenol <armagansnl@gmail.com>
2021-05-11 17:12:38 +0300

    App updated due to CORS errors v3

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 require('./db/mongoose');
+var cors = require('cors');
 const userRouter = require('./routers/userRouter');
 const taskRouter = require('./routers/taskRouter');
 
 const app = express();
 
 app.use(express.json());
+
+app.options('*', cors());
+app.use(cors());
+
 app.use(userRouter);
 app.use(taskRouter);
 app.use(require('cors')());
