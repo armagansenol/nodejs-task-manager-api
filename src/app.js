15f00deb34ec9e4ec981f9612ff37cd18aa45cde
armagansenol <armagansnl@gmail.com>
2021-05-11 17:00:16 +0300

    App updated due to CORS errors v2

--- src/app.js
+++ src/app.js
@@ -8,13 +8,6 @@ const app = express();
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
-
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
+app.use(require('cors')());
 
 module.exports = app;
